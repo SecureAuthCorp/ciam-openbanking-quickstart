014d6e9a48854249d6d364655c147f43ed8b0f16
Ireneusz Kawalec <ireneusz.kawalec@gmail.com>
2026-01-29 12:14:09 +0100

    fix

--- apps/configuration/templates_test.go
+++ apps/configuration/templates_test.go
@@ -63,9 +63,9 @@ func TestTemplates(t *testing.T) {
                    {
                      "alg":"RS256",
                      "e":"AQAB",
-                     "kid":"167467200346518873990055631921812347975180003245",
+                     "kid":"572356338587782038927312347033327560343951693321",
                      "kty":"RSA",
-                     "n":"4b_IX1bV29pw6_Ce8DdkoNx4dxJnDD9AyxmTG2z99cvlHG6BJaMF6l09ncGXGbv3dufDKrhftkwfbTBdpUEAeext_ugCmXTV06Fayva6Iq7xCNE8pA6hJT1y3Edsqq3IU8KVivYjYwd_vrSUfCe8pQRsR6K8rqnJ66ryn0yewkTEyCgPIv6pOMbgq1d5iX_2G9rZNhj74miN5y4fy0tsbI3q2RUOzt2d-htkoysqu3Xta6qPA3vEJ2FnQo3dhgw4XSCEvjz-HSGnsTC-XBv6j6jI9SD5jI2UYqnyDcYmRHPJx2sQ_c8aLYHRdZxrxqIxUzulS6g0x74E2m0gBMKF5w",
+					 "n": "s11Q7mwhtinAfo-HWGm_UTvXi6pNx1YkdhR8ech2L3Y0WAmb4c8C24I30nUYmafXU3xiDk4WJeZMEaOrS-Kia5Ynvv5ijddqAvwMWKuy7qwjLGupJTxNm2xUFxUjWvrcgc7d-OCyySbbFJSFcQ7pYwVzX9suzCYHE3TdZ0FR_czAHanRlHd91l7Dt_MkfKKnIGFL99YsUg13FeJ-qzH2WEoKYOLqljxcc-P-kC165oLe4nrFgTnYzd98J4Z7NYyyziX1Rbo5Tk08iEtHcwPqaIj-4iaueYUFcOpnR3ADQ7eTpBhKODuU0_Dx2gmtySsA-G-DJyAdIHPJE13J-Pb9gw",
                      "use":"sig"
                    }
                 ]
